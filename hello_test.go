a0a95881527f1d4061d10708d7ed744c8823dbcd
ShaunBillows <billows.shaun@gmail.com>
2023-02-28 15:07:03 +0000

    [feat] Hello support Spanish when passed as an arg

--- hello_test.go
+++ hello_test.go
@@ -13,21 +13,25 @@ import "testing"
 // 	}
 // }
 
-// Test with subtests
+// Test with sub tests
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("empty string defaults to 'world'", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
 		assertCorrectMessage(t, got, want)
 	})
-
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
 }
 
 // Test helper function 
