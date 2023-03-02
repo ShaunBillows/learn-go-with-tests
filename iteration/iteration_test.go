d3d175bf6d14286de035ffd0c3374240f02292f8
ShaunBillows <billows.shaun@gmail.com>
2023-03-02 18:10:59 +0000

    [test] Add benchmark test for Repeat

--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -10,3 +10,11 @@ func TestRepeat(t *testing.T) {
 		t.Errorf("expected %q but got %q", expected, repeated)
 	}
 }
+
+// benchmarks test code performance (Usage: 'go test -bench=')
+// runs b.N iterations (b.N is decided by the test suite)
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a")
+	}
+}
