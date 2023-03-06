ee3ab9dcefed5f0cf8d198c164cb359a647a8706
ShaunBillows <billows.shaun@gmail.com>
2023-03-06 15:07:20 +0000

    [feat] Create an Area methods for circle and rectangle types

--- structs-methods-interfaces/shapes.go
+++ structs-methods-interfaces/shapes.go
@@ -1,14 +1,20 @@
 package structs_methods_interfaces
 
+import "math"
+
 type Rectangle struct {
 	width  float64
 	height float64
 }
 
-func Perimeter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.height + rectangle.width)
+func (r Rectangle) Area() float64 {
+	return r.width * r.height
 }
 
-func Area(rectangle Rectangle) float64 {
-	return rectangle.width * rectangle.height
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
