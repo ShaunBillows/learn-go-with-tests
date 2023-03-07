ff243ad6e81860b1a364ee0957865a4a083a3603
ShaunBillows <billows.shaun@gmail.com>
2023-03-07 10:15:59 +0000

    [feat] Create Wallet struct with Deposit and Balance methods

--- pointers-errors/pointers.go
+++ pointers-errors/pointers.go
@@ -1,1 +1,17 @@
 package pointers
+
+import "fmt"
+
+type Wallet struct {
+	balance int
+}
+
+func (w Wallet) Deposit(amount int) {
+	fmt.Printf("address of balance in Deposit is %v \n", &w.balance)
+	w.balance += amount
+}
+
+func (w Wallet) Balance() int {
+	fmt.Printf("address of balance in Deposit is %v \n", &w.balance)
+	return w.balance
+}
