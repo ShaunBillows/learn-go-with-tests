0271cddc508521b37db315b88dfbdf5b5d7ada80
ShaunBillows <billows.shaun@gmail.com>
2023-03-27 14:24:18 +0100

    Add DI tutorial

--- dependency-injection/dependency_injection.go
+++ dependency-injection/dependency_injection.go
@@ -1,1 +1,23 @@
-package dependency_injection
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func Greet(writer io.Writer, name string) {
+	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
+func main() {
+	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(MyGreeterHandler)))
+}
+
+// fmt.Fprintf is like fmt.Printf but instead takes a Writer to send the string to,
+// whereas fmt.Printf defaults to stdout.
